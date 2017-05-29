fd096bca1ad0f430f24bdc88cf22090520289de9
Alex Samuel <alex@alexsamuel.net>
2017-05-29 16:03:20 -0400

    More tests.

--- strtod/test_parse_double.cc
+++ strtod/test_parse_double.cc
@@ -32,7 +32,17 @@ TEST(integers, parse_double_6) {
 }
 
 TEST(no_integer, parse_double_6) {
-  EXPECT_EQ(parse6(".125"), 0.125);
+  EXPECT_EQ(parse6( ".5"      ),  0.5);
+  EXPECT_EQ(parse6( ".125"    ),  0.125);
+  EXPECT_EQ(parse6( ".0078125"),  0.0078125);
+
+  EXPECT_EQ(parse6("+.5"      ),  0.5);
+  EXPECT_EQ(parse6("+.125"    ),  0.125);
+  EXPECT_EQ(parse6("+.0078125"),  0.0078125);
+
+  EXPECT_EQ(parse6("-.5"      ), -0.5);
+  EXPECT_EQ(parse6("-.125"    ), -0.125);
+  EXPECT_EQ(parse6("-.0078125"), -0.0078125);
 }
 
 TEST(huge, parse_double_6) {
