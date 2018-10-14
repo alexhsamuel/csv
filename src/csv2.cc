d1175521b07052bcf85d3f3bb03d8455e1c297e4
Alex Samuel <alex@alexsamuel.net>
2018-10-14 09:24:56 -0400

    More refactor.

--- src/csv2.cc
+++ src/csv2.cc
@@ -277,4 +277,53 @@ parse_array(
 }
 
 
+//------------------------------------------------------------------------------
+
+void
+print(
+  std::ostream& os,
+  Array const& arr,
+  bool const print_values)
+{
+  if (arr.variant() == Array::VARIANT_INT) {
+    auto const& int_arr = arr.int_arr();
+    os << "int64 column '" << int_arr.name 
+       << "' len=" << int_arr.len
+       << " min=" << int_arr.min << " max=" << int_arr.max << "\n";
+    if (print_values)
+      for (size_t i = 0; i < int_arr.len; ++i)
+        os << i << '.' << ' ' << int_arr.vals[i] << '\n';
+  }
+
+  else if (arr.variant() == Array::VARIANT_FLOAT) {
+    auto const& float_arr = arr.float_arr();
+    os << "float64 column '" << float_arr.name 
+       << "' len=" << float_arr.len
+       << " min=" << float_arr.min
+       << " max=" << float_arr.max << "\n";
+    if (print_values)
+      for (size_t i = 0; i < float_arr.len; ++i)
+        os << i << '.' << ' ' << float_arr.vals[i] << '\n';
+  }
+
+  else if (arr.variant() == Array::VARIANT_STRING) {
+    auto const& str_arr = arr.str_arr();
+    os << "str column len=" << str_arr.len
+       << " width=" << str_arr.width << '\n';
+    if (print_values)
+      for (size_t i = 0; i < str_arr.len; ++i) {
+        os << i << '.' << ' ' << '[';
+        char const* base = str_arr.chars.data();
+        for (char const* p = base + i * str_arr.width;
+             p < base + (i + 1) * str_arr.width;
+             ++p)
+          if (*p == 0)
+            os << "·";
+          else
+            os << *p;
+        os << ']' << '\n';
+      }
+  }
+}
+
 
