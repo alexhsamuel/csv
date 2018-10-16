c8e6cfcf4dadbc1d23783b57cdc3780bc6b734df
Alex Samuel <alex@alexsamuel.net>
2018-10-15 22:58:03 -0400

    Some loading logic.

--- tabcsv/ext/tabcsv.cc
+++ tabcsv/ext/tabcsv.cc
@@ -1,8 +1,64 @@
 #include <assert.h>
+#include <fcntl.h>
 #include <Python.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 
+#include "ThreadPool.hh"
 #include "csv2.hh"
 
+
+PyObject*
+load_file(
+  char const* const path)
+{
+  int const fd = open(path, O_RDONLY);
+  assert(fd >= 0);
+
+  struct stat info;
+  int res = fstat(fd, &info);
+  if (res != 0)
+    return PyErr_SetFromErrno(PyExc_IOError);
+  std::cout << "st_size = " << info.st_size << std::endl;
+  
+  void* ptr = mmap(nullptr, info.st_size, PROT_READ, MAP_SHARED, fd, 0);
+  if (ptr == MAP_FAILED)
+    return PyErr_SetFromErrno(PyExc_IOError);
+
+  Buffer buf{static_cast<char const*>(ptr), (size_t) info.st_size};
+
+  auto const cols = split_columns(buf);
+  std::cerr << "done with split\n";
+
+  {
+    ThreadPool pool(5);
+    std::vector<std::future<Array>> results;
+    // FIXME: Use std::transform.
+    for (auto const& col : cols)
+      results.push_back(pool.enqueue(parse_array, &col, true));
+
+    for (auto&& result : results)
+      std::cout << result.get();
+  }
+
+  res = munmap(ptr, info.st_size);
+  if (res != 0) {
+    PyErr_SetFromErrno(PyExc_IOError);
+    close(fd);
+    return NULL;
+  }
+
+  res = close(fd);
+  if (res != 0)
+    return PyErr_SetFromErrno(PyExc_IOError);
+
+  Py_INCREF(Py_None);
+  return Py_None;
+}
+
+
+
+//------------------------------------------------------------------------------
 
 extern "C" {
 
@@ -12,17 +68,14 @@ method_load_file(
   PyObject* const args,
   PyObject* const kw_args)
 {
-  static char* keywords[] = {"path", NULL};
+  static char const* const keywords[] = {"path", NULL};
   char const* path;
 
-  if (!PyArg_ParseTupleAndKeywords(args, kw_args, "s", keywords, &path))
+  if (!PyArg_ParseTupleAndKeywords(
+        args, kw_args, "s", (char**) keywords, &path))
     return NULL;
 
-  fprintf(stderr, "path: %s\n", path);
-  load_file(path);
-
-  Py_INCREF(Py_None);
-  return Py_None;
+  return load_file(path);
 }
 
 
